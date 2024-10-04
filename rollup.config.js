4ff269e38c650394376da76716c3947f1230ece9
Alexander Petree <apetree1001@email.phoenix.edu>
2024-10-04 06:20:31 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,6 @@
-export default () => {
-    return {
-        input: "main.js",
-        output: {
-            file: "dist/bundle.min.js",
-            format: "umd",
-            globals: {
-                "@jridgewell/source-map": "sourceMap",
-            },
-            name: "Terser",
-            sourcemap: false,
-            sourcemapExcludeSources: true,
-            esModule: false,
-            indent: false
-        },
-        external: "source-map",
-    };
-};
+export default () => { return { input: "main.js",
+        output: { file: "dist/bundle.min.js",
+            format: "umd", globals: {"@jridgewell/source-map": "sourceMap"},name: "Terser",sourcemap: false,
+            sourcemapExcludeSources: true, esModule: false, indent: false, external:"source-map"
+                };
+                     };
